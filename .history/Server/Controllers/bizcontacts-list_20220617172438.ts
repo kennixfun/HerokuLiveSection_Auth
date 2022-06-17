ae7019c75d7e31e3115dc3743a99fc50058266e2
Kennix Leung <kennixleung@Kennixs-MacBook-Pro.local>
2022-06-17 17:24:56 -0400

    added edit page

---
+++ .history/Server/Controllers/bizcontacts-list_20220617172438.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+
+import BusinessContacts from '../Models/bizcontacts';
+import { UserDisplayName } from '../Util';
+
+export function DisplayBizContactsList(req: express.Request, res: express.Response, next: express.NextFunction)
+{
+    // Accessing the database
+    BusinessContacts.find(function(err, bizContactsCollection)
+    {
+        if(err)
+        {
+            console.error(err);
+            res.end(err);
+        }
+        res.render('index', {title: 'Business Contacts List', page: 'bizcontacts-list', bizcontacts: bizContactsCollection, displayName: UserDisplayName(req)});
+    }).sort({FirstName: 1});
+}
+
+export function DisplayEditPage(req: express.Request, res: express.Response, next: express.NextFunction)
+{
+    res.render('index', { title: 'Edit Business Contacts' , page: 'edit', displayName: UserDisplayName(req)});
+}
