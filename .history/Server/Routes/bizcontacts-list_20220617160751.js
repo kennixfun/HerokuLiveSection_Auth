aa12c0c36fd073dd93410a6ed7c30bcaed8df818
Kennix Leung <kennixleung@Kennixs-MacBook-Pro.local>
2022-06-17 16:12:23 -0400

    updated business contacts

---
+++ .history/Server/Routes/bizcontacts-list_20220617160751.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const router = express_1.default.Router();
+const Util_1 = require("../Util");
+const bizcontacts_list_1 = require("../Controllers/bizcontacts-list");
+router.get('/bizcontacts-list', Util_1.AuthGuard, bizcontacts_list_1.DisplayBizContactsList);
+exports.default = router;
+//# sourceMappingURL=bizcontacts-list.js.map
